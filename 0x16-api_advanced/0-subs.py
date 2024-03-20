6c7119b5e4e94452a733c21ae0883fe13a572b16
Delbert-Kipyegon <delbertkip@gmail.com>
2024-03-20 07:02:46 -0700

    02

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+
+"""
+A script that queries the Reddit API and returns the number of subscribers
+for a given subreddit. If an invalid subreddit is given, the function returns 0.
+"""
 import requests
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/IndependenceMotor409)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """Returns the number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Python:subreddit.subscriber.counter:v1.0 (by /u/IndependenceMotor409)'}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get('data', {})
+            return data.get('subscribers', 0)
+        else:
+            return 0
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers", 0)
 
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
