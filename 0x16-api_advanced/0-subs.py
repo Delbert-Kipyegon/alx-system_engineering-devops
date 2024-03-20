a3dd801510b90acb94cac9bd2cb4f2f8322a25a9
Delbert-Kipyegon <delbertkip@gmail.com>
2024-03-20 06:56:05 -0700

    01

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,16 +3,14 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Python:subreddit.subscriber.counter:v1.0 (by /u/IndependenceMotor409)'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json().get('data', {})
-            return data.get('subscribers', 0)
-        else:
-            return 0
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/IndependenceMotor409)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers", 0)
+
